815950ba4fac8319386337d7a0097b7446fbf0d0
User <adam-daoud@live.com>
2025-02-21 11:29:49 +0100

    commit

--- src/Inlämning/ConverterTest.java
+++ src/Inlämning/ConverterTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.testng.Converter;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 
 public class ConverterTest {
 
